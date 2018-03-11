a5ba7dd2d2953ab018a4bb612da36d8436af17d3
ma7801 <ma7801@gmail.com>
2018-03-10 22:52:48 -0800

    3-10-18

---
+++ CalcHS/CalcHS/CalcButton.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcHS
+{
+    public enum ButtonType { Number, Operator, Function, Power, Grouping, Special };
+
+    public class CalcButton
+    {
+        public String buttonText;
+        public String displayText;
+        public String expressionText;
+        public ButtonType buttonType;
+        public int colSpan;
+        public int row;
+        public int col;
+        public Boolean displayable;
+ 
+        public CalcButton(ButtonType buttonType, int row, int col, String buttonText, String displayText, String expressionText, int colSpan)
+        { 
+            this.colSpan = colSpan;
+            this.row = row;
+            this.col = col;
+            this.buttonText = buttonText;
+            this.displayText = displayText;
+            this.expressionText = expressionText;
+            this.buttonType = buttonType;
+            displayable = true;
+        }
+
+        public CalcButton(ButtonType buttonType, int row, int col, String buttonText)
+        {
+            this.colSpan = 1;
+            this.row = row;
+            this.col = col;
+            this.buttonText = buttonText;
+            this.buttonType = buttonType;
+            displayable = false;
+        }
+
+        public CalcButton(ButtonType buttonType, int row, int col, String buttonText, int colSpan)
+        {
+            this.colSpan = colSpan;
+            this.row = row;
+            this.col = col;
+            this.buttonText = buttonText;
+            this.buttonType = buttonType;
+            displayable = false;
+        }
+
+
+        public CalcButton(ButtonType buttonType, int row, int col, String buttonText, String displayText, String expressionText)
+        {
+            this.colSpan = 1;
+            this.row = row;
+            this.col = col;
+            this.buttonText = buttonText;
+            this.displayText = displayText;
+            this.expressionText = expressionText;
+            this.buttonType = buttonType;
+            displayable = true;
+        }
+
+        
+
+    }
+}
--- CalcHS/CalcHS/MainPage.xaml.cs
+++ CalcHS/CalcHS/MainPage.xaml.cs
@@ -3,83 +3,165 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using NCalc;
+using NCalc;
 using Xamarin.Forms;
 
 namespace CalcHS
 {
+    public enum DisplayState { Inputting, Answer };
+
     public partial class MainPage : ContentPage
     {
-        public String display;
-        public Dictionary<String, CalcButton> exprButtons;  // Indexed by text on button
-        
+        Editor display;
+        DisplayState displayState;
+        String expressionText;
+        String answer;
+        private List<Button> buttons;
+        private List<CalcButton> calcButtons;
+        private Dictionary<System.Guid, CalcButton> buttonDict = new Dictionary<System.Guid, CalcButton> { };  // Hash lookup table for click events
+
 
         public MainPage()
         {
-            InitializeComponent();
-            display = "";
+            //InitializeComponent();  // Gets UI from XAML? -- not using anymore
 
-            // Initialize the "expression" buttons - i.e. any buttons that add on to an expression
-            CalcButton sinButton = new CalcButton("sin(", "SIN(");
-            CalcButton cosButton = new CalcButton("cos(", "COS(");
-            CalcButton tanButton = new CalcButton("tan(", "TAN(");
-            CalcButton logButton = new CalcButton("log(", "LOG(");
-            CalcButton lnButton = new CalcButton("ln(", "LN(");
-            CalcButton sqrtButton = new CalcButton("\u221a(", "SQRT(");
-            //CalcButton expButton = new CalcButton("^", )
+            //BindingContext = new Calculator();
 
+            answer = "";
 
+            calcButtons = new List<CalcButton>
+            {
+                new CalcButton(ButtonType.Special, 0, 0, "Inv"),
+                new CalcButton(ButtonType.Special, 0, 1, "Table"),
+                new CalcButton(ButtonType.Special, 0, 2, "Stat"),
+                new CalcButton(ButtonType.Special, 0, 3, "Clear", 2),
+                new CalcButton(ButtonType.Function, 1, 0, "sin", "sin(", "Sin("),
+                new CalcButton(ButtonType.Function, 1, 1, "cos", "cos(", "Cos("),
+                new CalcButton(ButtonType.Function, 1, 2, "tan", "tan(", "Tan("),
+                new CalcButton(ButtonType.Function, 1, 3, "log", "log(", "Log("),
+                new CalcButton(ButtonType.Function, 1, 4, "ln", "ln(", "Ln("),
+                new CalcButton(ButtonType.Special, 2, 0, "Deg/Rad"),
+                new CalcButton(ButtonType.Function, 2, 1, "\u221a", "\u221a(", "Sqrt("),
+                new CalcButton(ButtonType.Power, 2, 2, "^", "^(", "Pow("),   
+                new CalcButton(ButtonType.Power, 2, 3, "x^2", "^(2)", "^(2)"),
+                new CalcButton(ButtonType.Special, 2, 4, "Del"),
+                new CalcButton(ButtonType.Number, 3, 0, "7", "7", "7"),
+                new CalcButton(ButtonType.Number, 3, 1, "8", "8", "8"),
+                new CalcButton(ButtonType.Number, 3, 2, "9", "9", "9"),
+                new CalcButton(ButtonType.Grouping, 3, 3, "(", "(", "("),
+                new CalcButton(ButtonType.Grouping, 3, 4, ")", ")", ")"),
+                new CalcButton(ButtonType.Number, 4, 0, "4", "4", "4"),
+                new CalcButton(ButtonType.Number, 4, 1, "5", "5", "5"),
+                new CalcButton(ButtonType.Number, 4, 2, "6", "6", "6"),
+                new CalcButton(ButtonType.Operator, 4, 3, "\u00d7", "\u00d7", "*"),
+                new CalcButton(ButtonType.Operator, 4, 4, "\u00f7", "\u00f7", "/"),
+                new CalcButton(ButtonType.Number, 5, 0, "1", "1", "1"),
+                new CalcButton(ButtonType.Number, 5, 1, "2", "2", "2"),
+                new CalcButton(ButtonType.Number, 5, 2, "3", "3", "3"),
+                new CalcButton(ButtonType.Operator, 5, 3, "+", "+", "+"),
+                new CalcButton(ButtonType.Operator, 5, 4, "\u2212", "\u2212", "-"),
+                new CalcButton(ButtonType.Operator, 6, 0, "%", "%", "%"),
+                new CalcButton(ButtonType.Number, 6, 1, "0", "0", "0"),
+                new CalcButton(ButtonType.Number, 6, 2, ".", ".", "."),
+                new CalcButton(ButtonType.Number, 6, 3, "(-)", "-", "-"),
+                new CalcButton(ButtonType.Special, 6, 4, "Enter\n(=)")
+            };
+            
 
+            display = new Editor
+            {
+                Text = ""
 
+            };
 
+            var grid = new Grid();
 
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            grid.Children.Add(display, 0, 0);   // Add display to grid
+
+            // Add the buttons
+            buttons = new List<Button> { };
+          
+            for (int i = 0; i < calcButtons.Count; i++) {
+                buttons.Add(new Button
+                {
+                    Text = calcButtons[i].buttonText
+                });
+                
+                if (calcButtons[i].displayable)
+                {
+                    buttons[i].Clicked += AppendToDisplay;
+                }
+                else
+                {
+                    switch(calcButtons[i].buttonText)
+                    {
+                        case ("Clear"):
+                            buttons[i].Clicked += ClearDisplay;
+                            break;
+                        case ("Del"):
+                            buttons[i].Clicked += BackSpace;
+                            break;
+                        case ("Inv"):
+                            buttons[i].Clicked += ActivateInverse;
+                            break;
+                        case ("Deg/Rad"):
+                            buttons[i].Clicked += ToggleDegRad;
+                            break;
+                        case ("Enter\n(=)"):
+                            buttons[i].Clicked += ParseAndEvalDisplay;
+                            break;
+                    }
+                }
+
+                // Add to hash to lookup later for click events:
+                buttonDict.Add(buttons[i].Id, calcButtons[i]);
+                
+                // Add to grid:
+                grid.Children.Add(buttons[i], calcButtons[i].col, calcButtons[i].row+1);
+                Grid.SetColumnSpan(buttons[i], calcButtons[i].colSpan);
+
+            }
+
+            Content = grid;
+        
+            
+          
         }
 
         private void AppendToDisplay(object sender, EventArgs e)
         {
             Button button = (Button)sender;
-            String textToAppend;
 
-            // Exceptions for buttons whose text doesn't quite match what should be in the display:
-            switch (button.Text)
+            /*if(displayState == DisplayState.Answer)
             {
-                case ("\u221a"):  // Square Root symbol
-                    textToAppend = "\u221a(";
-                    break;
-                case ("x^2"):
-                    textToAppend = "^(2)";
-                    break;
-                case ("^"):
-                    textToAppend = "^(";
-                    break;
-                case ("(-)"):
-                    textToAppend = "-";
-                    break;
-                case ("sin"):
-                    textToAppend = "sin(";
-                    break;
-                case ("cos"):
-                    textToAppend = "cos(";
-                    break;
-                case ("tan"):
-                    textToAppend = "tan(";
-                    break;
-                case ("log"):
-                    textToAppend = "log(";
-                    break;
-                case ("ln"):
-                    textToAppend = "ln(";
-                    break;
-                default:
-                    textToAppend = button.Text;
-                    break;
-            }
+                ClearDisplay(null, null);
+            }*/
 
-            display += textToAppend;
-            textDisplay.Text = display;
+            if (buttonDict[button.Id].buttonType == ButtonType.Operator && expressionText == "" && answer != "")
+            {
+                display.Text += "ans";
+                expressionText = answer;
+            }
+            display.Text += buttonDict[button.Id].displayText;
+            expressionText += buttonDict[button.Id].expressionText;
+            
         }
 
-        private void ActivateInverse(object sender, EventArgs e)
+        public void ActivateInverse(object sender, EventArgs e)
         {
 
         }
@@ -96,25 +178,26 @@ namespace CalcHS
 
         private void ClearDisplay(object sender, EventArgs e)
         {
-            display = "";
-            textDisplay.Text = display;
-
+            display.Text = "";
+            expressionText = "";
         }
 
 
         private void ParseAndEvalDisplay(object sender, EventArgs e)
         {
-            //Expression exp = new Expression(display);
-            //String temp = exp.Evaluate().ToString();
-            //textDisplay.Text = temp;
-            //display = "";
+            Expression exp = new Expression(expressionText);
+            answer = exp.Evaluate().ToString();
+            display.Text += "\n" + answer + "\n";
+            expressionText = "";
+            //displayState = DisplayState.Answer;
+
+            
 
         }
 
         private void BackSpace(object sender, EventArgs e)
         {
-            display = display.Substring(0, display.Length - 1);
-            textDisplay.Text = display;
+            display.Text = display.Text.Substring(0, display.Text.Length - 1);
         }
 
         private void ToggleDegRad(object sender, EventArgs e)
@@ -123,33 +206,5 @@ namespace CalcHS
         }
     }
 
-    public class CalcButton
-    {
-        public String displayText;
-        public String expressionText;
-
-        /*
-        public CalcButton(int orderNum, String displayText, String expressionText, int columnSpan)
-        {
-            this.orderNum = orderNum;
-            this.columnSpan = columnSpan;
-            this.displayText = displayText;
-            this.expressionText = expressionText;
-        }
-        
-        public CalcButton(int orderNum)
-        {
-            this.orderNum = orderNum;
-            this.columnSpan = 1;
-            this.displayText = "";
-            this.expressionText = "";
-        }
-        */
-        public CalcButton(String displayText, String expressionText)
-        {
-            this.displayText = displayText;
-            this.expressionText = expressionText;
-        }
-
-    }
+    
 }
